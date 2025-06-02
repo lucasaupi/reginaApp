76374e8d9df3fca4ba159f1106853d1859e798b7
Lucas <lucas-aupi@hotmail.com>
2025-06-02 17:50:51 -0300

    main color in app name

--- lib/presentation/widgets/main_scaffold.dart
+++ lib/presentation/widgets/main_scaffold.dart
@@ -93,9 +93,7 @@ class MainScaffold extends ConsumerWidget {
 
                     if (confirmed == true) {
                       await ref.read(authControllerProvider.notifier).logout();
-                      ref.invalidate(
-                        appointmentProvider,
-                      ); // Limpia turnos al cerrar sesión
+                      ref.invalidate(appointmentProvider);
                       context.go('/login');
                     }
                   },
